2e57c9154d62866b416ac4c2a2d995971006c4d7
mozboz <lewis.james@gmail.com>
2014-06-14 00:39:06 +0200

    xmlhttprequest method, imported editor

--- oddProfileClient.js
+++ oddProfileClient.js
@@ -1,3 +1,5 @@
+
+
 function OddProfileClient(appUri) {
 
     throwIfUndefined(appUri, "appUri");
@@ -97,20 +99,89 @@ OddProfileContext.prototype.putContent = function(key, data) {
 
 }
 
+/**
+ *
+ * @param url       Profile root URL
+ * @param method
+ * @param data      Object of key:value pairs that will be appended to URL
+ * @return {Promise}
+ */
+function promiseXhrRequest(url, method, data) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+
+        var queryItemsString;
+        var params;
+
+        if (typeof data != 'undefined') {
+            var queryItems = [];
+            for (key in data) {
+                if (data.hasOwnProperty(key)) {
+                    if (data[key] == null) {
+                        queryItems.push(key);
+                    } else {
+                        queryItems.push(key + '=' + value);
+                    }
+                }
+            }
+            queryItemsString = queryItems.join('&');
+        }
+
+        var req = new XMLHttpRequest();
+        req.setRequestHeader("Accept", getApiVersionString());
+
+        switch (method) {
+            case "POST", "PUT":
+                params = queryItemsString;
+                req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                req.setRequestHeader("Content-length", params.length);
+                break;
+
+            case "GET", "HEAD":
+                url = url + '?' + queryItemsString;
+                break;
+        }
+
+        req.open(method, url, true);
+
+        req.onload = function() {
+            // This is called even on 404 etc
+            // so check the status
+            if (req.status == 200) {
+                // Resolve the promise with the response text
+                resolve(req);
+            }
+            else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject(Error(req));
+            }
+        };
+
+        // Handle network errors
+        req.onerror = function() {
+            reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send(params);
+    });
+}
+
 function throwIfUndefined(paramValue, paramName) {
 
     if (typeof paramValue == "undefined")
-        throw "The " + paramName ?? "" + " parameter must be set!";
+        throw "The " + paramName + " parameter must be set!";
 }
 
 function throwIfNotT(paramValue, paramName, expectedType) {
 
     if (typeof paramValue != expectedType)
-        throw "The " + paramName ?? "" + " parameter must be of type " + expectedType + "! Current type is: "
+        throw "The " + paramName + " parameter must be of type " + expectedType + "! Current type is: "
                     + typeof paramValue;
 }
 
 function throwIfNotObject(paramValue, paramName) {
     if (typeof yourVariable != 'object')
-        throw "The " + paramName ?? "" + " parameter must be a object!";
+        throw "The " + paramName + " parameter must be a object!";
 }
